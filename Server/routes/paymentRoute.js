1220087d8b8b55ac769a688ab06c61ed5a03a85a
Yokesh <yokeshananthan@gmail.com>
2024-12-19 20:22:50 +0530

    Update bcryptjs

--- Server/routes/paymentRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/initiate-payment', paymentController.initiatePayment);
-router.post('/payment-response', paymentController.paymentResponse);
-
-module.exports = router;
