44d268846f1a0ebdee3b0b6d6b1463a73963a040
Yokesh <yokeshananthan@gmail.com>
2024-12-19 20:56:27 +0530

    Update mongoose

--- Server/config/db.js
+++ Server/config/db.js
@@ -5,7 +5,7 @@ const MongoDB_URI = process.env.MONGODB_URI;
 // console.log(MongoDB_URI);
 
 const connectMongo = () => {
-  mongoose.connect(MongoDB_URI)
+  mongoose.connect(MongoDB_URI, { userNewUrlParser: true, unifiedTopology: true })
     .then(() => console.log('MongoDb is connected successfully'))
     .catch((err) => console.error(err));
 }
